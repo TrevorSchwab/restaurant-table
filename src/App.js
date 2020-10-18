e4f9ae52ddb43c8b6279f1dff5c2916bdbc27479
Trevor Schwab <trevschwab@gmail.com>
2020-10-18 18:20:07 -0500

    Revert "Moved Restaurant Table into a separate <Header /> component"
    This reverts commit f01ff2fad36b607a404c79cf6bd966afa6c28b90.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Header from './components/Header';
+import { STATES, GENRES } from './constants';
 import Pagination from './components/Pagination';
 import './App.css';
 
@@ -68,8 +68,50 @@ const App = () => {
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
-    <div>
-      <Header handleChange={handleChange} />
+    <div className="App">
+      <div className="filter-controls">
+        <div className="search-container col-5">
+          <form name="Search" onSubmit={(e) => handleChange(e)}>
+            <label>
+              <input
+                placeholder="Search Restaurants"
+                name="Search"
+                id="onChangeSearch"
+                onChange={(e) => handleChange(e)}
+              />
+            </label>
+            <input className="filterInputs" type="submit" value="Submit" />
+          </form>
+        </div>
+        <div className="dropdown-controls col-7">
+          <label className="input-labels">
+            <span>State:</span>
+            <select className="filter-inputs" name="State" onChange={(e) => handleChange(e)}>
+              <option value="all">All</option>
+              {STATES.map((state) => {
+                return (
+                  <option key={state} value={state}>
+                    {state}
+                  </option>
+                );
+              })}
+            </select>
+          </label>
+          <label className="input-labels">
+            <span>Genre:</span>
+            <select className="filter-inputs" name="Genre" onChange={(e) => handleChange(e)}>
+              <option value="all">All</option>
+              {GENRES.map((genre) => {
+                return (
+                  <option key={genre} value={genre}>
+                    {genre}
+                  </option>
+                );
+              })}
+            </select>
+          </label>
+        </div>
+      </div>
       <table className="restaurant-table">
         <tbody>
           <tr>
