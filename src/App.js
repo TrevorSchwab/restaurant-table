3d7b1e33e113d5f6fe18125f1e510fb7a70adea7
Trevor Schwab <trevschwab@gmail.com>
2020-10-18 18:03:04 -0500

    Filter inputs are now styled and responsive
    - Simple flex adjustments, margins, etc.
    - Used @media queries for smartphones, tablets, and desktop

--- src/App.js
+++ src/App.js
@@ -69,44 +69,48 @@ const App = () => {
 
   return (
     <div className="App">
-      <div>
-        <form name="Search" onSubmit={(e) => handleChange(e)}>
-          <label>
-            <input
-              placeholder="Search Restaurants"
-              name="Search"
-              id="onChangeSearch"
-              onChange={(e) => handleChange(e)}
-            />
+      <div className="filter-controls">
+        <div className="search-container col-5">
+          <form name="Search" onSubmit={(e) => handleChange(e)}>
+            <label>
+              <input
+                placeholder="Search Restaurants"
+                name="Search"
+                id="onChangeSearch"
+                onChange={(e) => handleChange(e)}
+              />
+            </label>
+            <input className="filterInputs" type="submit" value="Submit" />
+          </form>
+        </div>
+        <div className="dropdown-controls col-7">
+          <label className="input-labels">
+            <span>State:</span>
+            <select className="filter-inputs" name="State" onChange={(e) => handleChange(e)}>
+              <option value="all">All</option>
+              {STATES.map((state) => {
+                return (
+                  <option key={state} value={state}>
+                    {state}
+                  </option>
+                );
+              })}
+            </select>
           </label>
-          <input type="submit" value="Search" />
-        </form>
-        <label>
-          <span>State:</span>
-          <select name="State" onChange={(e) => handleChange(e)}>
-            <option value="all">All</option>
-            {STATES.map((state) => {
-              return (
-                <option key={state} value={state}>
-                  {state}
-                </option>
-              );
-            })}
-          </select>
-        </label>
-        <label>
-          <span>Genre:</span>
-          <select name="Genre" onChange={(e) => handleChange(e)}>
-            <option value="all">All</option>
-            {GENRES.map((genre) => {
-              return (
-                <option key={genre} value={genre}>
-                  {genre}
-                </option>
-              );
-            })}
-          </select>
-        </label>
+          <label className="input-labels">
+            <span>Genre:</span>
+            <select className="filter-inputs" name="Genre" onChange={(e) => handleChange(e)}>
+              <option value="all">All</option>
+              {GENRES.map((genre) => {
+                return (
+                  <option key={genre} value={genre}>
+                    {genre}
+                  </option>
+                );
+              })}
+            </select>
+          </label>
+        </div>
       </div>
       <table>
         <tbody>
