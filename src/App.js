b14a62c995311224609f9315a623c64f83c56782
Trevor Schwab <trevschwab@gmail.com>
2020-10-18 14:28:33 -0500

    Now rendering all 50 States in a dropdown
    - Populating dropdown with the constant array STATES

--- src/App.js
+++ src/App.js
@@ -1,6 +1,59 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 
+const STATES = [
+  'AL',
+  'AK',
+  'AZ',
+  'AR',
+  'CA',
+  'CO',
+  'CT',
+  'DE',
+  'FL',
+  'GA',
+  'HI',
+  'ID',
+  'IL',
+  'IN',
+  'IA',
+  'KS',
+  'KY',
+  'LA',
+  'ME',
+  'MD',
+  'MA',
+  'MI',
+  'MN',
+  'MS',
+  'MO',
+  'MT',
+  'NE',
+  'NV',
+  'NH',
+  'NJ',
+  'NM',
+  'NY',
+  'NC',
+  'ND',
+  'OH',
+  'OK',
+  'OR',
+  'PA',
+  'RI',
+  'SC',
+  'SD',
+  'TN',
+  'TX',
+  'UT',
+  'VT',
+  'VA',
+  'WA',
+  'WV',
+  'WI',
+  'WY',
+];
+
 const App = () => {
   const [restaurantData, setRestaurantData] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -23,6 +76,21 @@ const App = () => {
   }, []);
   return (
     <div className="App">
+      <div>
+        <label>
+          <span>State:</span>
+          <select name="State">
+            <option value="all">All</option>
+            {STATES.map((state) => {
+              return (
+                <option key={state} value={state}>
+                  {state}
+                </option>
+              );
+            })}
+          </select>
+        </label>
+      </div>
       <table>
         <tbody>
           <tr>
